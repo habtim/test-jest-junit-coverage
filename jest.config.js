65aeb0b422486e943174dec362beb92f6a401e4d
habtim <66038477+habtim@users.noreply.github.com>
2023-12-13 15:33:16 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/add.ts'],
-    coverageDirectory: "coverage",
-    coverageReporters: [
-        "text",
-        "html",
-        "text-lcov",
-        "text-summary"
-    ],
-    reporters: [
-        "default",
-        ["jest-junit", { usePathForSuiteName: "true" }]
-    ]
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/sum.ts'],
+  coverageDirectory: "coverage",
+  coverageReporters: [
+    "text",
+    "html",
+    "text-summary"
+  ],
+  reporters: [
+    "default",
+    ["jest-junit", { usePathForSuiteName: "true" }]
+  ]
 };
